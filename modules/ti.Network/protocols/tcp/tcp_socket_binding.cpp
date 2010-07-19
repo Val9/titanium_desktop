236fbd6a6bdc080248025446c9543eebdc93eb88
Josh Roesslein <jroesslein@appcelerator.com>
2010-07-19 15:21:06 -0500

    Begin work on fixing up TCP sockets.

--- modules/ti.Network/protocols/tcp/tcp_socket_binding.cpp
+++ modules/ti.Network/protocols/tcp/tcp_socket_binding.cpp
@@ -12,169 +12,32 @@
 
 namespace ti
 {
-	static kroll::Logger* GetLogger()
+	TCPSocketBinding::TCPSocketBinding(std::string host, int port) :
+		KEventObject("Network.TCPSocket"),
+		host(host), port(port)
 	{
-		return kroll::Logger::Get("Network.TCPSocket");
-	}
-
-	TCPSocketBinding::TCPSocketBinding(Host* ti_host, std::string host, int port) :
-		StaticBoundObject("Network.TCPSocket"),
-		ti_host(ti_host),
-		host(host),
-		port(port),
-		opened(false), 
-		currentSendDataOffset(0),
-		readStarted(false),
-		onRead(0),
-		onWrite(0),
-		onTimeout(0),
-		onReadComplete(0),
-		readObserver(*this, &TCPSocketBinding::ReadyForRead),
-		writeObserver(*this, &TCPSocketBinding::ReadyForWrite),
-		timeoutObserver(*this, &TCPSocketBinding::OnTimeout),
-		errorObserver(*this, &TCPSocketBinding::OnError),
-		writeReadyHandlerInstalled(false),
-		connectAdapter(new Poco::RunnableAdapter<TCPSocketBinding>(
-			*this, &TCPSocketBinding::ConnectThread))
-	{
-		/**
-		 * @tiapi(method=True,name=Network.TCPSocket.connect,since=0.2)
-		 * @tiapi Connect the Socket object to the host specified during creation.
-		 * @tiapi The connection will be made asynchronously. Use onError
-		 * @tiapi to detect failures.
-		 */
 		this->SetMethod("connect", &TCPSocketBinding::Connect);
-
-		/**
-		 * @tiapi(method=True,name=Network.TCPSocket.close,since=0.2)
-		 * Close the Socket object's connection, if it is open.
-		 * @tiresult[Boolean] true if the connection was successfully close, false if otherwise
-		 */
+		this->SetMethod("setKeepAlive", &TCPSocketBinding::SetKeepAlive);
 		this->SetMethod("close", &TCPSocketBinding::Close);
-
-		/**
-		 * @tiapi(method=True,name=Network.TCPSocket.write,since=0.2)
-		 * @tiapi Write data to the Socket's connection, if open.
-		 * @tiarg[String, data] The data to write to the connection.
-		 * @tiresult[Boolean] True if the write was successful, false otherwise.
-		 */
 		this->SetMethod("write", &TCPSocketBinding::Write);
-
-		/**
-		 * @tiapi(method=True,name=Network.TCPSocket.isClosed,since=0.2)
-		 * @tiapi Check whether the Socket is closed.
-		 * @tiresult[Boolean] true if a Socket object is closed, false if otherwise
-		 */
-		this->SetMethod("isClosed",&TCPSocketBinding::IsClosed);
-
-		/**
-		 * @tiapi(method=True,name=Network.TCPSocket.onRead,since=0.2)
-		 * @tiapi Set a callback that will be fired when data is received on the Socket.
-		 * @tiarg[Function, onRead] Function to be called when data is received.
-		 */
-		this->SetMethod("onRead",&TCPSocketBinding::SetOnRead);
-
-		/**
-		 * @tiapi(method=True,name=Network.TCPSocket.onWrite,since=0.2)
-		 * @tiapi Set a callback that will be fired when data is written on the Socket.
-		 * @tiarg[Function, onWrite] Function to be called when data is written.
-		 */
-		this->SetMethod("onWrite",&TCPSocketBinding::SetOnWrite);
-
-		/**
-		 * @tiapi(method=True,name=Network.TCPSocket.onTimeout,since=0.2)
-		 * @tiapi Set the callback that will be fired when an operation times out on the Socket.
-		 * @tiarg[Function, onTimeout] Function to be called when an operation times out.
-		 */
-		this->SetMethod("onTimeout",&TCPSocketBinding::SetOnTimeout);
-
-		/**
-		 * @tiapi(method=True,name=Network.TCPSocket.onError,version=0.5)
-		 * @tiapi Set the callback that will be fired when the Socket encounters an error.
-		 * @tiarg[Function, onError] Function to be called when an error happens.
-		 */
-		this->SetMethod("onError",&TCPSocketBinding::SetOnError);
-
-		/**
-		 * @tiapi(method=True,name=Network.TCPSocket.onReadComplete,since=0.2)
-		 * @tiapi Set the callback function that will be fired when a read finishes. A read is
-		 * @tiapi considered finished if some bytes have been read and a subsequent call to read
-		 * @tiapi returns zero bytes.
-		 * @tiarg[Function, onReadComplete] Function be called when a read completes.
-		 */
-		this->SetMethod("onReadComplete",&TCPSocketBinding::SetOnReadComplete);
-
-		// Attach the reactor event handlers
-		this->reactor.addEventHandler(this->socket, readObserver);
-		this->reactor.addEventHandler(this->socket, timeoutObserver);
-		this->reactor.addEventHandler(this->socket, errorObserver);
 	}
 
 	TCPSocketBinding::~TCPSocketBinding()
 	{
-		// Remove the reactor event handlers
-		this->reactor.removeEventHandler(this->socket, readObserver);
-		this->reactor.removeEventHandler(this->socket, writeObserver);
-		this->reactor.removeEventHandler(this->socket, timeoutObserver);
-		this->reactor.removeEventHandler(this->socket, errorObserver);
-
-		if (this->opened)
-		{
-			this->reactor.stop();
-			this->socket.close();
-		}
-
-		connectThread.join();
-		delete connectAdapter;
+		// TODO: Remove the reactor event handlers
 	}
 
-	void TCPSocketBinding::SetOnRead(const ValueList& args, KValueRef result)
+	void TCPSocketBinding::Connect(const ValueList& args, KValueRef result)
 	{
-		this->onRead = args.at(0)->ToMethod();
-	}
-
-	void TCPSocketBinding::SetOnWrite(const ValueList& args, KValueRef result)
-	{
-		this->onWrite = args.at(0)->ToMethod();
-	}
-
-	void TCPSocketBinding::SetOnTimeout(const ValueList& args, KValueRef result)
-	{
-		this->onTimeout = args.at(0)->ToMethod();
-	}
-
-	void TCPSocketBinding::SetOnError(const ValueList& args, KValueRef result)
-	{
-		this->onError = args.at(0)->ToMethod();
-	}
-
-	void TCPSocketBinding::SetOnReadComplete(const ValueList& args, KValueRef result)
-	{
-		this->onReadComplete = args.at(0)->ToMethod();
-	}
-
-	void TCPSocketBinding::IsClosed(const ValueList& args, KValueRef result)
-	{
-		return result->SetBool(!this->opened);
-	}
-
-	void TCPSocketBinding::ConnectThread()
-	{
-		static std::string error("Connection failed: ");
 		try
 		{
-			// Give the connetion process and the reactor the same timeout.
-			this->socket.connect(Poco::Net::SocketAddress(
-				this->host, this->port), this->timeout);
+			// Make connection without blocking
+			Poco::Net::SocketAddress address(this->host, this->port);
+			this->socket.connectNB(address, this->timeout);
 
 			// Enable Keepalive mode on this socket. See:
 			// http://tldp.org/HOWTO/TCP-Keepalive-HOWTO/index.html
 			this->socket.setKeepAlive(true);
-
-			this->reactor.setTimeout(this->timeout);
-			this->thread.start(this->reactor);
-			this->opened = true;
-			return;
 		}
 		catch (Poco::IOException &e)
 		{
@@ -191,6 +54,12 @@ namespace ti
 
 		if (!this->onError.isNull())
 			RunOnMainThread(this->onError, ValueList(Value::NewString(error)), false);
+	}
+
+	void TCPSocketBinding::SetKeepAlive(const ValueList& args, KValueRef result)
+	{
+		args.VerifyException("setKeepAlive", "b");
+		this->socket.setKeepAlive(args.GetBool(0));
 	}
 
 	void TCPSocketBinding::Connect(const ValueList& args, KValueRef result)
--- modules/ti.Network/protocols/tcp/tcp_socket_binding.h
+++ modules/ti.Network/protocols/tcp/tcp_socket_binding.h
@@ -34,56 +34,24 @@
 
 namespace ti
 {
-	class TCPSocketBinding : public StaticBoundObject
+	class TCPSocketBinding : public KEventObject
 	{
 	public:
-		TCPSocketBinding(Host *ti_host, std::string host, int port);
+		TCPSocketBinding(std::string& host, int port);
 		virtual ~TCPSocketBinding();
 
 	private:
-		Host* ti_host;
 		std::string host;
 		int port;
-		Poco::Net::StreamSocket socket;
-		Poco::Net::SocketReactor reactor;
-		Poco::Thread thread;
-		bool opened;
-		Poco::Timespan timeout;
-
-		std::queue<BytesRef> sendData;
-		Poco::Mutex sendDataMutex;
-		size_t currentSendDataOffset;
-		bool readStarted;
-
-		KMethodRef onRead;
-		KMethodRef onWrite;
-		KMethodRef onTimeout;
-		KMethodRef onError;
-		KMethodRef onReadComplete;
-
-		Poco::NObserver<TCPSocketBinding, Poco::Net::ReadableNotification> readObserver;
-		Poco::NObserver<TCPSocketBinding, Poco::Net::WritableNotification> writeObserver;
-		Poco::NObserver<TCPSocketBinding, Poco::Net::TimeoutNotification> timeoutObserver;
-		Poco::NObserver<TCPSocketBinding, Poco::Net::ErrorNotification> errorObserver;
-		bool writeReadyHandlerInstalled;
-
-		Poco::Thread connectThread;
-		Poco::RunnableAdapter<TCPSocketBinding>* connectAdapter;
-
+		Poco::StreamSocket socket;
 
 		void Connect(const ValueList& args, KValueRef result);
-		void ConnectThread();
+		void SetKeepAlive(const ValueList& args, KValueRef result);
 		void Write(const ValueList& args, KValueRef result);
 		void Close(const ValueList& args, KValueRef result);
-		void IsClosed(const ValueList& args, KValueRef result);
-		void SetOnRead(const ValueList& args, KValueRef result);
-		void SetOnWrite(const ValueList& args, KValueRef result);
-		void SetOnTimeout(const ValueList& args, KValueRef result);
-		void SetOnError(const ValueList& args, KValueRef result);
-		void SetOnReadComplete(const ValueList& args, KValueRef result);
 
-		void ReadyForRead(const Poco::AutoPtr<Poco::Net::ReadableNotification>& n);
-		void ReadyForWrite(const Poco::AutoPtr<Poco::Net::WritableNotification>& n);
+		void OnRead(const Poco::AutoPtr<Poco::Net::ReadableNotification>& n);
+		void OnWrite(const Poco::AutoPtr<Poco::Net::WritableNotification>& n);
 		void OnTimeout(const Poco::AutoPtr<Poco::Net::TimeoutNotification>& n);
 		void OnError(const Poco::AutoPtr<Poco::Net::ErrorNotification>& n);
 	};
