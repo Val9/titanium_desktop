040f8fe3d6f3482d8f1de5cbc05d7bde4d98fcfb
Josh Roesslein <jroesslein@appcelerator.com>
2010-07-20 18:10:04 -0500

    More TCP socket work.

--- modules/ti.Network/protocols/tcp/tcp_socket_binding.cpp
+++ modules/ti.Network/protocols/tcp/tcp_socket_binding.cpp
@@ -1,30 +1,26 @@
 /**
  * Appcelerator Titanium - licensed under the Apache Public License 2
  * see LICENSE in the root folder for details on the license.
- * Copyright (c) 2008 Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2010 Appcelerator, Inc. All Rights Reserved.
  */
 #include "tcp_socket_binding.h"
-#include <Poco/NObserver.h>
-#include <Poco/RunnableAdapter.h>
-#include <kroll/kroll.h>
 
-#define BUFFER_SIZE 1024   // choose a reasonable size to send back to JS
+#include <Poco/NObserver.h>
 
 namespace ti
 {
 	TCPSocketBinding::TCPSocketBinding(std::string host, int port) :
 		KEventObject("Network.TCPSocket"),
-		host(host), port(port)
+		address(host, port)
 	{
 		this->SetMethod("connect", &TCPSocketBinding::Connect);
-		this->SetMethod("setKeepAlive", &TCPSocketBinding::SetKeepAlive);
 		this->SetMethod("close", &TCPSocketBinding::Close);
 		this->SetMethod("write", &TCPSocketBinding::Write);
+		this->SetMethod("setKeepAlive", &TCPSocketBinding::SetKeepAlive);
 	}
 
 	TCPSocketBinding::~TCPSocketBinding()
 	{
-		// TODO: Remove the reactor event handlers
 	}
 
 	void TCPSocketBinding::Connect(const ValueList& args, KValueRef result)
@@ -34,26 +30,20 @@ namespace ti
 			// Make connection without blocking
 			Poco::Net::SocketAddress address(this->host, this->port);
 			this->socket.connectNB(address, this->timeout);
+		}
+		catch (Poco::IOException& e)
+		{
+			this->FireErrorEvent(e.what());
+		}
+	}
 
-			// Enable Keepalive mode on this socket. See:
-			// http://tldp.org/HOWTO/TCP-Keepalive-HOWTO/index.html
-			this->socket.setKeepAlive(true);
-		}
-		catch (Poco::IOException &e)
-		{
-			error.append(e.displayText());
-		}
-		catch (std::exception &e)
-		{
-			error.append(e.what());
-		}
-		catch (...)
-		{
-			error.append("Unknown exception");
-		}
+	void TCPSocketBinding::Close(const ValueList& args, KValueRef result)
+	{
+	}
 
-		if (!this->onError.isNull())
-			RunOnMainThread(this->onError, ValueList(Value::NewString(error)), false);
+	void TCPSocketBinding::Write(const ValueList& args, KValueRef result)
+	{
+		args.VerifyException("write", "s");
 	}
 
 	void TCPSocketBinding::SetKeepAlive(const ValueList& args, KValueRef result)
@@ -62,192 +52,20 @@ namespace ti
 		this->socket.setKeepAlive(args.GetBool(0));
 	}
 
-	void TCPSocketBinding::Connect(const ValueList& args, KValueRef result)
+	void TCPSocketBinding::OnRead(const Poco::AutoPtr<Poco::Net::ReadableNotification>& n)
 	{
-		int timeout = 10;
-		if (args.size() > 0)
-		{
-			timeout = args.at(0)->ToInt();
-		}
-
-		if (this->opened || connectThread.isRunning())
-		{
-			throw ValueException::FromString("Socket is already open");
-		}
-
-		result->SetBool(true);
-		this->timeout = Poco::Timespan(timeout, 0);
-		connectThread.start(*connectAdapter);
 	}
 
-	void TCPSocketBinding::ReadyForRead(
-		const Poco::AutoPtr<Poco::Net::ReadableNotification>& n)
+	void TCPSocketBinding::OnWrite(const Poco::AutoPtr<Poco::Net::WritableNotification>& n)
 	{
-		static std::string eprefix("TCPSocketBinding::OnRead: ");
-
-		try
-		{
-			// Always read bytes, so that the tubes get cleared.
-			char data[BUFFER_SIZE + 1];
-			int size = socket.receiveBytes(&data, BUFFER_SIZE);
-
-			// A read is only complete if we've already read some bytes from the socket.
-			bool readComplete = this->readStarted && (size <= 0);
-			this->readStarted = (size > 0);
-
-			if (readComplete && !this->onReadComplete.isNull())
-			{
-				ValueList args;
-				RunOnMainThread(this->onReadComplete, args, false);
-			}
-			else if (size > 0 && !this->onRead.isNull())
-			{
-				data[size] = '\0';
-
-				BytesRef bytes(new Bytes(data, size));
-				ValueList args(Value::NewObject(bytes));
-				RunOnMainThread(this->onRead, args, false);
-			}
-		}
-		catch (ValueException& e)
-		{
-			GetLogger()->Error("Read failed: %s", e.ToString().c_str());
-			ValueList args(Value::NewString(e.ToString()));
-
-			if (!this->onError.isNull())
-				RunOnMainThread(this->onError, args, false);
-		}
-		catch (Poco::Exception &e)
-		{
-			GetLogger()->Error("Read failed: %s", e.displayText().c_str());
-			ValueList args(Value::NewString(e.displayText()));
-
-			if (!this->onError.isNull())
-				RunOnMainThread(this->onError, args, false);
-		}
-		catch (...)
-		{
-			GetLogger()->Error("Read failed: unknown exception");
-			ValueList args(Value::NewString("Unknown exception during read"));
-
-			if (!this->onError.isNull())
-				RunOnMainThread(this->onError, args, false);
-		}
 	}
 
-	void TCPSocketBinding::ReadyForWrite(
-		const Poco::AutoPtr<Poco::Net::WritableNotification>& n)
+	void TCPSocketBinding::OnTimeout(const Poco::AutoPtr<Poco::Net::TimeoutNotification>& n)
 	{
-		if (sendData.empty())
-			return;
-
-		BytesRef buffer(0);
-		{
-			Poco::Mutex::ScopedLock lock(sendDataMutex);
-			buffer = sendData.front();
-		}
-
-		const char* data = buffer->Get() + currentSendDataOffset;
-		size_t length = buffer->Length() - currentSendDataOffset;
-		size_t count = this->socket.sendBytes(data, length);
-		currentSendDataOffset += count;
-
-		if (currentSendDataOffset == (size_t) buffer->Length())
-		{
-			// Only send the onWrite message when we've exhausted a Bytes.
-			if (!this->onWrite.isNull())
-			{
-				ValueList args(Value::NewInt(buffer->Length()));
-				RunOnMainThread(this->onWrite, args, false);
-			}
-
-			Poco::Mutex::ScopedLock lock(sendDataMutex);
-			sendData.pop();
-			currentSendDataOffset = 0;
-
-			// Uninstall the ReadyForWrite reactor handler, because it will push
-			// the CPU to 100% usage if there is nothing to write.
-			if (sendData.size() == 0)
-			{
-				this->reactor.removeEventHandler(this->socket, writeObserver);
-				writeReadyHandlerInstalled = false;
-			}
-		}
 	}
 
-	void TCPSocketBinding::OnTimeout(
-		const Poco::AutoPtr<Poco::Net::TimeoutNotification>& n)
+	void TCPSocketBinding::OnError(const Poco::AutoPtr<Poco::Net::ErrorNotification>& n)
 	{
-		if (this->onTimeout.isNull())
-		{
-			return;
-		}
-		RunOnMainThread(this->onTimeout, ValueList(), false);
-	}
-
-	void TCPSocketBinding::OnError(
-		const Poco::AutoPtr<Poco::Net::ErrorNotification>& n)
-	{
-		if (this->onError.isNull())
-		{
-			return;
-		}
-		ValueList args(Value::NewString(n->name()));
-		RunOnMainThread(this->onError, args, false);
-	}
-
-	void TCPSocketBinding::Write(const ValueList& args, KValueRef result)
-	{
-		args.VerifyException("Write", "o|s");
-
-		static std::string eprefix("TCPSocketBinding::Write: ");
-		if (!this->opened && !this->connectThread.isRunning())
-			throw ValueException::FromString(eprefix +  "Socket is not open");
-
-		BytesRef data(0);
-		if (args.at(0)->IsString())
-		{
-			std::string sendString(args.GetString(0));
-			data = new Bytes(sendString.c_str(), sendString.size());
-		}
-		else if (args.at(0)->IsObject())
-		{
-			KObjectRef dataObject(args.GetObject(0));
-			data = dataObject.cast<Bytes>();
-		}
-
-		if (data.isNull())
-			throw ValueException::FromString("Cannot send non-Bytes object");
-
-		{
-			Poco::Mutex::ScopedLock lock(sendDataMutex);
-			sendData.push(data);
-
-			// Only install the ReadyForWrite handler when there is actually data
-			// to write, because otherwise the CPU usage will spike to 100%
-			if (!writeReadyHandlerInstalled)
-			{
-				this->reactor.addEventHandler(this->socket, writeObserver);
-				writeReadyHandlerInstalled = true;
-			}
-		}
-
-		result->SetBool(true);
-	}
-
-	void TCPSocketBinding::Close(const ValueList& args, KValueRef result)
-	{
-		if (this->opened)
-		{
-			this->opened = false;
-			this->reactor.stop();
-			this->socket.close();
-			result->SetBool(true);
-		}
-		else
-		{
-			result->SetBool(false);
-		}
 	}
 }
 
--- modules/ti.Network/protocols/tcp/tcp_socket_binding.h
+++ modules/ti.Network/protocols/tcp/tcp_socket_binding.h
@@ -1,36 +1,18 @@
 /**
  * Appcelerator Titanium - licensed under the Apache Public License 2
  * see LICENSE in the root folder for details on the license.
- * Copyright (c) 2008 Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2008-2010 Appcelerator, Inc. All Rights Reserved.
  */
 
 #ifndef _TCP_SOCKET_BINDING_H_
 #define _TCP_SOCKET_BINDING_H_
 
-#ifndef WINVER
-#define WINVER 0x0501
-#endif
-
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0501
-#endif
-
-#ifndef _WIN32_WINDOWS
-#define _WIN32_WINDOWS 0x0410
-#endif
-
-#ifndef _WIN32_IE
-#define _WIN32_IE 0x600
-#endif
-
-#include <kroll/kroll.h>
 #include <Poco/Thread.h>
 #include <Poco/Net/StreamSocket.h>
 #include <Poco/Net/SocketReactor.h>
 #include <Poco/Net/SocketNotification.h>
-#include <Poco/NObserver.h>
-#include <Poco/RunnableAdapter.h>
-#include <queue>
+
+#include <kroll/kroll.h>
 
 namespace ti
 {
@@ -40,20 +22,19 @@ namespace ti
 		TCPSocketBinding(std::string& host, int port);
 		virtual ~TCPSocketBinding();
 
-	private:
-		std::string host;
-		int port;
-		Poco::StreamSocket socket;
-
 		void Connect(const ValueList& args, KValueRef result);
-		void SetKeepAlive(const ValueList& args, KValueRef result);
-		void Write(const ValueList& args, KValueRef result);
 		void Close(const ValueList& args, KValueRef result);
+		void Write(const ValueList& args, KValueRef result);
+		void SetKeepAlive(const ValueList& args, KValueRef result);
 
 		void OnRead(const Poco::AutoPtr<Poco::Net::ReadableNotification>& n);
 		void OnWrite(const Poco::AutoPtr<Poco::Net::WritableNotification>& n);
 		void OnTimeout(const Poco::AutoPtr<Poco::Net::TimeoutNotification>& n);
 		void OnError(const Poco::AutoPtr<Poco::Net::ErrorNotification>& n);
+
+	private:
+		Poco::Net::SocketAddress address;
+		Poco::Net::StreamSocket socket;
 	};
 }
 
